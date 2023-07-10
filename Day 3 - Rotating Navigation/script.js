0a27394c91afb1ef2fec58fa574b7d4d54a764b7
TimmiKau <125255142+TimmiKau@users.noreply.github.com>
2023-07-10 09:21:21 +0200

    a

---
+++ Day 3 - Rotating Navigation/script.js
@@ -0,0 +1,19 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () =>
+    container.classList.add('show-nav')
+)
+
+/*close.addEventListener('click', () =>
+    container.classList.remove('show-nav')
+) */
+
+// Had to try my code worked as well! 
+
+close.addEventListener(('click'), () => {
+
+    container.classList.remove('show-nav')
+
+})
